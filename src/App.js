92d07ad926553c281af7ea74401515f21dc30b12
Sara <saragonzalezburgin@hotmail.com>
2020-02-17 16:47:43 +0100

    Shaped the component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ function App() {
       <Cover />
       <Box color={color} />
       <button
+        style={{
+          display: 'block',
+          marginTop: -170,
+          marginLeft: 760,
+          fontSize: 20,
+          color: 'white',
+          backgroundColor: 'gray',
+          marginRight: -300,
+          borderRadius: 100,
+          padding: 50,
+          marginBottom: 100,
+        }}
         onClick={function onClick() {
           setColor(randomColor());
         }}
--- src/Box.js
+++ src/Box.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Box(props) {
   return (
-    <div style={{ background: props.color }}>Hex code - {props.color}</div>
+    <div
+      style={{
+        background: props.color,
+        height: 200,
+        width: 400,
+        marginLeft: 300,
+      }}
+    >
+      Hex code - {props.color}
+    </div>
   );
 }
-
 export default Box;
